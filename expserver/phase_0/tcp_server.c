f7095c8c73a4f87c1b4f33f24523eb106792dcce
Xplant2006 <parthivsatish2@gmail.com>
2026-08-04 10:52:47 +0530

    Phase 0 Stage 4

--- expserver/phase_0/tcp_server.c
+++ expserver/phase_0/tcp_server.c
@@ -6,10 +6,12 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <sys/epoll.h>
 
 #define PORT 8080
 #define BUFF_SIZE 10000
 #define MAX_ACCEPT_BACKLOG 5
+#define MAX_POLL_EVENTS 10
 
 void stringreverse(char *str){
     for(int start=0,end=strlen(str)-2;start<end;start++,end--){
@@ -41,40 +43,60 @@ int main(){
     listen(listen_sock_fd,MAX_ACCEPT_BACKLOG);
     printf("[INFO] Server listening on port %d\n", PORT);
 
-    
+    //Setting up epoll
+    int epoll_fd=epoll_create1(0);
+
+    struct epoll_event event, events[MAX_POLL_EVENTS];
+
+    event.events=EPOLLIN;
+    event.data.fd=listen_sock_fd;
+    epoll_ctl(epoll_fd,EPOLL_CTL_ADD,listen_sock_fd,&event);
 
     
     while(1){
-        //Setting up client address struct and accepting connection
-        struct sockaddr_in client_addr;
-        socklen_t client_addr_len;
+        printf("[DEBUG] Epoll wait\n");
+        int n_read_fds=epoll_wait(epoll_fd,events,MAX_POLL_EVENTS,-1);
+        
+        for(int i=0;i<n_read_fds;i++){
+            int curr_fd=events[i].data.fd;
 
-        int conn_sock_fd=accept(listen_sock_fd, (struct sockaddr *)&client_addr,&client_addr_len);
-        printf("[INFO] Client connected to server\n");
+            if(curr_fd==listen_sock_fd){
+                //Setting up client address struct and accepting connection
+                struct sockaddr_in client_addr;
+                socklen_t client_addr_len=sizeof(client_addr);
 
-        //Setting up receiving loop
-        while(1){
-            char buff[BUFF_SIZE];
+                int conn_sock_fd=accept(listen_sock_fd, (struct sockaddr *)&client_addr,&client_addr_len);
+                printf("[INFO] Client connected to server\n");
 
-            memset(buff,0,BUFF_SIZE);
-
-            ssize_t read_n = recv(conn_sock_fd,buff,sizeof(buff),0);
-
-            if(read_n<0){
-                printf("[INFO] Error occured. Closing connection\n");
-                close(conn_sock_fd);
-                break;
+                struct epoll_event conn_event;
+                conn_event.events=EPOLLIN;
+                conn_event.data.fd=conn_sock_fd;
+                epoll_ctl(epoll_fd,EPOLL_CTL_ADD,conn_sock_fd,&conn_event);
             }
-            else if(read_n==0){
-                printf("[INFO] Client Disconnected. Closing connection\n");
-                close(conn_sock_fd);
-                break;
+            else{
+                char buff[BUFF_SIZE];
+
+                memset(buff,0,BUFF_SIZE);
+
+                ssize_t read_n = recv(curr_fd,buff,sizeof(buff),0);
+
+                if(read_n<0){
+                    printf("[INFO] Error occured. Closing connection\n");
+                    close(curr_fd);
+                    continue;
+                }
+                else if(read_n==0){
+                    printf("[INFO] Client Disconnected. Closing connection\n");
+                    close(curr_fd);
+                    continue;
+                }
+                printf("[CLIENT MESSAGE] %s",buff);
+
+                stringreverse(buff);
+
+                send(curr_fd,buff,read_n,0);
             }
-            printf("[CLIENT MESSAGE] %s",buff);
-
-            stringreverse(buff);
-
-            send(conn_sock_fd,buff,read_n,0);
         }
+            
     }
 }
